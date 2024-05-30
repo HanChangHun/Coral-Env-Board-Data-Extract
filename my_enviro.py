01860757c4fa1c505981e5b70d5609fe4a03fd26
HanChanghun <ehwjs1914@naver.com>
2024-05-30 20:19:00 +0900

    add log table logic

--- my_enviro.py
+++ my_enviro.py
@@ -12,40 +12,63 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
+import json
+from time import sleep
 import RPi.GPIO as GPIO
 from coral.enviro.board import EnviroBoard
-from coral.cloudiot.core import CloudIot
-from luma.core.render import canvas
 from PIL import Image, ImageDraw, ImageFont
-from time import sleep
 
-import argparse
-import itertools
-import os
-import signal
-
-import json
 import pymysql
+import signal
+import sys
 
 
-with open("mysql_config.json", "r") as f:
+def signal_handler(sig, frame):
+    print("Exiting program...")
+    db.close()
+    GPIO.cleanup()
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
+
+with open("/home/pi/workspaces/env_board/mysql_config.json", "r") as f:
     db_config = json.load(f)
 
-db = pymysql.connect(
-    host=db_config['host'],
-    user=db_config['user'], 
-    db=db_config['db'],
-    password=db_config['password'],
-    charset='utf8'
-)
+
+def connect_to_db():
+    while True:
+        try:
+            db = pymysql.connect(
+                host=db_config["host"],
+                user=db_config["user"],
+                db=db_config["db"],
+                password=db_config["password"],
+                charset="utf8",
+            )
+            return db
+        except pymysql.err.OperationalError as e:
+            print(f"Error: {e}. Retrying in 1 seconds...")
+            sleep(1)  # 1초 대기 후 재시도
+        except Exception as e:
+            print(f"Error: {e}. Retrying in 1 seconds...")
+            sleep(1)  # 1초 대기 후 재시도
+
+
+db = connect_to_db()
 
 BUTTON_PIN = 23
-DEFAULT_CONFIG_LOCATION = os.path.join(os.path.dirname(__file__), "cloud_config.ini")
+FONT_PATH = "/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf"
+FONT_SIZE = 12
 
+delete_interval = "2 HOUR"
 button_pressed = False
 
 
-def update_display(display, msg, font, rotate=True):
+def update_display(display, msg, font, rotate=False):
     canvas = Image.new("1", (display.width, display.height), color=0)
     draw = ImageDraw.Draw(canvas)
     draw.text((0, 0), msg, fill=1, font=font)
@@ -64,7 +87,8 @@ def button_callback(channel):
     global button_pressed
     button_pressed = not button_pressed
 
-def insert_with_retry(cursor, query, values):
+
+def execute_with_retry(cursor, query, values):
     while True:
         try:
             cursor.execute(query, values)
@@ -79,6 +103,68 @@ def insert_with_retry(cursor, query, values):
             sleep(1)  # 1초 대기 후 재시도
 
 
+def upload_data(cursor, sensors):
+    query_log = """
+        INSERT INTO env_board_log (device_id, ambient_light, pressure, temperature, humidity)
+        VALUES (%s, %s, %s, %s, %s)
+    """
+
+    values_log = (
+        "env_board_1",
+        sensors["ambient_light"],
+        sensors["pressure"],
+        sensors["temperature"],
+        sensors["humidity"],
+    )
+
+    query = """
+        INSERT INTO env_board (device_id, ambient_light, pressure, temperature, humidity)
+        VALUES (%s, %s, %s, %s, %s)
+        ON DUPLICATE KEY UPDATE
+            ambient_light = VALUES(ambient_light),
+            pressure = VALUES(pressure),
+            temperature = VALUES(temperature),
+            humidity = VALUES(humidity)
+    """
+
+    values = (
+        "env_board_1",
+        sensors["ambient_light"],
+        sensors["pressure"],
+        sensors["temperature"],
+        sensors["humidity"],
+    )
+
+    execute_with_retry(cursor, query_log, values_log)
+    execute_with_retry(cursor, query, values)
+
+
+def delete_old_data(cursor):
+    query_delete = f"DELETE FROM env_board_log WHERE timestamp < DATE_SUB(NOW(), INTERVAL {delete_interval})"
+    execute_with_retry(cursor, query_delete, None)
+
+
+def setup_button():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(
+        BUTTON_PIN, GPIO.FALLING, callback=button_callback, bouncetime=200
+    )
+
+
+def get_font():
+    return ImageFont.truetype(FONT_PATH, FONT_SIZE)
+
+
+def read_sensors(enviro):
+    return {
+        "ambient_light": enviro.ambient_light,
+        "pressure": enviro.pressure,
+        "temperature": enviro.temperature,
+        "humidity": enviro.humidity,
+    }
+
+
 def main():
     # Pull arguments from command line.
     parser = argparse.ArgumentParser(description="Enviro Kit Demo")
@@ -89,70 +175,41 @@ def main():
         default=0.5,
     )
     parser.add_argument(
-        "--upload_delay", help="Cloud upload delay (seconds)", type=int, default=1
+        "--upload_delay",
+        help="Cloud upload delay (seconds)",
+        type=int,
+        default=1,
     )
     parser.add_argument(
-        "--cloud_config", help="Cloud IoT config file", default=DEFAULT_CONFIG_LOCATION
+        "--rotate",
+        action="store_true",
+        default=False,
     )
     args = parser.parse_args()
 
-    font_path = "/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf"
-    font_size = 12
-    font = ImageFont.truetype(font_path, font_size)
+    font = get_font()
+    setup_button()
 
-    # Button setting
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(
-        BUTTON_PIN, GPIO.FALLING, callback=button_callback, bouncetime=200
-    )
-
-    # Create instances of EnviroKit and Cloud IoT.
     enviro = EnviroBoard()
     cursor = db.cursor()
 
-    with CloudIot(args.cloud_config) as cloud:
-        sensors = {}
-        read_period = int(args.upload_delay / (2 * args.display_duration))
-        for read_count in itertools.count():
-            sensors["ambient_light"] = enviro.ambient_light
-            sensors["pressure"] = enviro.pressure
-            sensors["temperature"] = enviro.temperature
-            sensors["humidity"] = enviro.humidity
-            
-            query = """
-                INSERT INTO env_board (device_id, ambient_light, pressure, temperature, humidity)
-                VALUES (%s, %s, %s, %s, %s)
-                ON DUPLICATE KEY UPDATE
-                    ambient_light = VALUES(ambient_light),
-                    pressure = VALUES(pressure),
-                    temperature = VALUES(temperature),
-                    humidity = VALUES(humidity)
-            """
+    while True:
+        sleep(1e-4)
+        sensors = read_sensors(enviro)
 
-            values = ('env_board_1', sensors["ambient_light"], sensors["pressure"], sensors["temperature"], sensors["humidity"])
+        upload_data(cursor, sensors)
+        delete_old_data(cursor)
 
-            insert_with_retry(cursor, query, values)
-            db.commit()
+        msg = ""
+        if button_pressed:
+            msg = f"Light: {_none_to_nan(sensors['ambient_light']):.2f} lux\n"
+            msg += f"Pressure: {_none_to_nan(sensors['pressure']):.2f} kPa"
+        else:
+            msg = f"Temp: {_none_to_nan(sensors['temperature']):.2f} C\n"
+            msg += f"RH: {_none_to_nan(sensors['humidity']):.2f} %"
+        update_display(enviro.display, msg, font, rotate=args.rotate)
 
-            if button_pressed:
-                # 조도와 기압 출력
-                msg = "Light: %.2f lux\n" % _none_to_nan(sensors["ambient_light"])
-                msg += "Pressure: %.2f kPa" % _none_to_nan(sensors["pressure"])
-                update_display(enviro.display, msg, font, rotate=False)
-            else:
-                # 온도와 습도 출력
-                msg = "Temp: %.2f C\n" % _none_to_nan(sensors["temperature"])
-                msg += "RH: %.2f %%" % _none_to_nan(sensors["humidity"])
-                update_display(enviro.display, msg, font, rotate=False)
-
-            if read_count % read_period == 0 and cloud.enabled():
-                cloud.publish_message(sensors)
-
-            sleep(args.display_duration)
-            
-    db.close()
-    GPIO.cleanup()
+        sleep(args.display_duration)
 
 
 if __name__ == "__main__":
